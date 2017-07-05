a092a4d54f8458db4dc9075adb86043ad76969b0
Peter Schoener <peter.schoener@student.uni-tuebingen.de>
2017-07-06 01:41:09 +0200

    forgot to update header

--- hash_map_io.h
+++ hash_map_io.h
@@ -12,6 +12,6 @@ int hash_map_read(FILE* stream, hash_map* map, size_t key_sz, size_t value_sz);
 int hash_map_fast_read(FILE* stream, hash_map* map, size_t key_sz, size_t value_sz);
 
 int hash_map_custom_write(FILE* stream, hash_map* map, int(* write_fn)(FILE* stream, void* key, void* value));
-int hash_map_custom_read(FILE* stream, hash_map* map, int(* read_fn)(FILE* stream, void* key, void* value));
+int hash_map_custom_read(FILE* stream, hash_map* map, int(* read_fn)(FILE* stream, void** key, void** value));
 
 #endif
